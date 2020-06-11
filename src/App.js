e63364dff53bf5d838cd98507f39c8ac287cd5dc
asadadi786 <57137231+asadadi786@users.noreply.github.com>
2020-06-11 21:24:52 +0500

    remove h1 tag

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div >
-       <h1> Asad Hello World from react</h1>
+        Asad Hello World from react
               </div>
     );
   }
