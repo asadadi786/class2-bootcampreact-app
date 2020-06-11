1ba0ac15932eaaa67bf3fbf1e5007b25eebecd5a
asadadi786 <57137231+asadadi786@users.noreply.github.com>
2020-06-11 21:17:07 +0500

    Add my name

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div >
-       <h1> Hello World from react</h1>
+       <h1> Asad Hello World from react</h1>
               </div>
     );
   }
